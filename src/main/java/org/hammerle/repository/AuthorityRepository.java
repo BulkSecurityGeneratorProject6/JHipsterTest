6b9b074ef79635cfb208c926b9ea29b4fdb48e1a
Bensense <43452220+Bensense@users.noreply.github.com>
2019-04-24 14:54:56 +0200

    Initial application generated by JHipster-5.8.2

---
+++ src/main/java/org/hammerle/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package org.hammerle.repository;
+
+import org.hammerle.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/org/hammerle/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,20 @@
+package org.hammerle.security;
+
+import org.hammerle.config.Constants;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of AuditorAware based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM_ACCOUNT));
+    }
+}
